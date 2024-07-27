3497e78c5fd453d483f3e7083b6cd3abd00ad8ce
acaValkyrie <acavalkyrie@gmail.com>
2024-07-28 01:38:58 +0900

    delete comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,3 @@
-// #include <avr/io.h>
-// #include <util/delay.h>
-
-// int main(){
-//   // A Port 0 set to OUTPUT
-//   DDRA = 0b0001;
-
-//   while(1){
-//     // A Port 0 set to HIGH
-//     PORTA = 0b0001;
-//     _delay_ms(1000);
-
-//     // A Port 0 set to LOW
-//     PORTA = 0b0000;
-//     _delay_ms(1000);
-//   }
-//   return 0;
-// }
-
 #include <Arduino.h>
 
 const int led_pin = 2;
@@ -33,6 +14,4 @@ void loop(){
     analogWrite(led_pin, l);
     delay(interval_ms);
   }
-  // digitalWrite(led_pin, HIGH);
-  // delay(interval_ms);
 }
